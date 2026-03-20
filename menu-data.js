6513439f35f2a816b12583086f4a82e384c211d9
felipelorenzo01 <felipe.lorenzo01@gmail.com>
2026-03-20 16:51:27 -0300

    Update menu-data.js

--- menu-data.js
+++ menu-data.js
@@ -58,7 +58,7 @@ var MENU = [
     categoria: "Tapioca",
     itens: [
       { nome: "Tapioca Fumeiro",               preco: 29.90, desc: "Tapioca recheada com carne de fumeiro artesanal",                       foto: "tapioca-fumeiro",    badge: "p" },
-      { nome: "Tapioca Filé & Cogumelo",       preco: 36.90, desc: "Filé mignon com cogumelos frescos",                           foto: "tapioca-file",                   badge: "" },
+      { nome: "Tapioca Filé & Cogumelo",       preco: 38.90, desc: "Filé mignon com cogumelos frescos",                           foto: "tapioca-file",                   badge: "" },
       { nome: "Tapioca Camarão",               preco: 42.90, desc: "Camarões salteados com pesto e tomate",                       foto: "tapioca-camarao",    badge: "" },
       { nome: "Tapioca Mista",                 preco: 24.90, desc: "Queijo derretido e presunto na tapioca",                      foto: "tapioca-mista",      badge: "" },
       { nome: "Tapioca Veggie",                preco: 22.90, desc: "Legumes grelhados, queijo e tomate",                          foto: "tapioca-veggie",     badge: "" },
